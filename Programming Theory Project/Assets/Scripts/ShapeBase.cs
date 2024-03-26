a9e738f2d332029f4329c348d3daa4cf06df9f45
czeromax <myhollandemail@gmail.com>
2024-03-26 12:32:22 +0100

    Simple Sphere
    - Added a simple sphere with a base shape class that includes a color and physics based mouse interaction

---
+++ Programming Theory Project/Assets/Scripts/ShapeBase.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShapeBase : MonoBehaviour, IPointerDownHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerUpHandler
+{
+    private Color color = Color.red;
+    public Color Color { get => color; set => color = value; }
+
+    public string ShapeName { get => shapeName; set => shapeName = value; }
+    private string shapeName = "Base Shape";
+
+    public Material myMaterial;
+
+    private void Awake()
+    {
+        //myMaterial = gameObject.GetComponent<Material>();
+        myMaterial.color = color;
+    }
+    //These functions handle mouse interaction
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        myMaterial.color = Color.green;
+    }
+
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        myMaterial.color = color;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //blanck
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //blanck
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //blanck
+    }
+}
